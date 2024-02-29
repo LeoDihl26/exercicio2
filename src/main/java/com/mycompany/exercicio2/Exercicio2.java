811e0435ca6f5c443f7c5c8a7672b646c0c7f5f9
aluno <leodihl@gmail.com>
2024-02-29 19:39:43 -0300

    Exercicio de Par ou Ímpar

---
+++ src/main/java/com/mycompany/exercicio2/Exercicio2.java
@@ -0,0 +1,48 @@
+package com.mycompany.exercicio2;
+
+import java.util.Scanner;
+
+public class Exercicio2 {
+
+    public static void main(String[] args) {
+        
+        int N;
+
+        Scanner teclado = new Scanner(System.in);
+        
+        System.out.println("É Par ou Ímpar?");
+
+        N = teclado.nextInt();
+        
+        if (N % 2 == 0) {
+            System.out.println("Ele é Par, amiguinho.");
+        } 
+        else {
+            System.out.println("Ele é Ímpar, amiguinho.");
+        }
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+    }
+}
